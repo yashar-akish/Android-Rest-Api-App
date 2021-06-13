251e1cf8ee5441c21313b0c8ef4ef2a39f474146
yashar-akish <yashar_akish@yahoo.com>
2021-06-13 00:16:28 -0400

    adding required codes to read from API server

--- app/src/main/java/com/company/retrofit_rest_api/MainActivity.java
+++ app/src/main/java/com/company/retrofit_rest_api/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
         textC = findViewById(R.id.textViewC);
         textD = findViewById(R.id.textViewD);
 
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        RetrofitApi retrofitApi = retrofit.create(RetrofitApi.class);
+
+        Call<List<ModelClass>> call = retrofitApi.getModelClass();
+
+        call.enqueue(new Callback<List<ModelClass>>() {
+            @Override
+            public void onResponse(Call<List<ModelClass>> call, Response<List<ModelClass>> response) {
+                if (!response.isSuccessful()){
+                    textA.setText("error");
+                    textB.setText("error");
+                    textC.setText("error");
+                    textD.setText("error");
+                }
+
+                List<ModelClass> data = response.body();
+
+                textA.setText(""+data.get(0).getUserId());
+                textB.setText(""+data.get(0).getId());
+                textC.setText(""+data.get(0).getTitle());
+                textD.setText(""+data.get(0).getSubTitle());
+            }
+
+            @Override
+            public void onFailure(Call<List<ModelClass>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "There is an error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
