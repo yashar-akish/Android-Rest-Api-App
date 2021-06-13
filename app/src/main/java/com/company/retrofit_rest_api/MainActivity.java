e0b3b00865e4922dd1aa14886425a4f2de744abd
yashar-akish <yashar_akish@yahoo.com>
2021-06-12 23:45:37 -0400

    design and required library (retrofit)

--- app/src/main/java/com/company/retrofit_rest_api/MainActivity.java
+++ app/src/main/java/com/company/retrofit_rest_api/MainActivity.java
@@ -3,12 +3,22 @@ package com.company.retrofit_rest_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textA, textB, textC, textD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textA = findViewById(R.id.textViewA);
+        textB = findViewById(R.id.textViewB);
+        textC = findViewById(R.id.textViewC);
+        textD = findViewById(R.id.textViewD);
+
+
     }
 }
